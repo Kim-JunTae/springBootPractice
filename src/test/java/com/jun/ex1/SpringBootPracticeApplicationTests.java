748bc76877d5b9780ea98068d034cc7c98ce97fe
juntae.kim <juntae.kim@i-screamedu.co.kr>
2021-07-30 22:28:05 +0900

    Initial commit

---
+++ src/test/java/com/jun/ex1/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jun.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads...");
+    }
+
+}
